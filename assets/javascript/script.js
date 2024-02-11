6ee9a0303e417e2dc42d286f6d42ae4508698f4c
Alex Ryder <ajryder31@gmail.com>
2024-02-11 14:16:04 -0700

    math to calculate player hp

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,6 @@
 //fetch("https://youtube.googleapis.com/youtube/v3/playlists?key=AIzaSyCXMsxFGkkJjCZmQmL7b1zKT0XQ-NBiCIE")
-var level = 0;
+//player stats
+var level = 3;
 var hp = 0;
 var charClass = "warlock";
 var ac = 0;
@@ -10,9 +11,31 @@ var int = 0;
 var wis = 0;
 var cha = 0;
 
+//enemy stats
+var cr = 0;
+var ehp = 0;
+var eac = 0;
+var estr = 0;
+var edex = 0;
+var econ = 0;
+var eint = 0;
+var ewis = 0;
+var echa = 0;
+
 
 //this function fills the player info
 function fillStats(){
+    if(charClass == "barbarian"){
+        hp = (7 * (level - 1)) + (con * level) + 12;
+    }else if(charClass == "fighter" || charClass == "paladin"){
+        hp = (6 * (level - 1)) + (con * level) + 10;
+    }else if(charClass == "bard" || charClass == "cleric" || charClass == "druid" || charClass == "monk" || charClass == "rouge" || charClass == "sorcerer" || charClass == "warlock"){
+        hp = (5 * (level - 1)) + (con * level) + 8;
+    }else if(charClass == "wizard"){
+        hp = (4 * (level - 1)) + (con * level) + 6;
+    }else {
+        hp = 1;
+    }
     $("#level").text("Level: " + level);
     $("#class").text("Class: " + charClass);
     $("#hp").text("HP: " + hp);
@@ -27,21 +50,31 @@ function fillStats(){
 
 //this fucntion fills in the info for the enemy
 function enemyStats(){
-    $("#cr").text("Level: " + level);
-    $("#ehp").text("HP: " + hp);
-    $("#eac").text("AC: " + ac);
-    $("#estr").text("Str: " + str);
-    $("#edex").text("Dex: " + dex);
-    $("#econ").text("Con: " + con);
-    $("#eint").text("Int: " + int);
-    $("#ewis").text("wis: " + wis);
-    $("#echa").text("cha: " + cha);
+    //cr = ;
+    $("#cr").text("Level: " + cr);
+    //ehp = ;
+    $("#ehp").text("HP: " + ehp);
+    //eac = ;
+    $("#eac").text("AC: " + eac);
+    //estr = ;
+    $("#estr").text("Str: " + estr);
+    //edex = ;
+    $("#edex").text("Dex: " + edex);
+    //econ = ;
+    $("#econ").text("Con: " + econ);
+    //eint = ;
+    $("#eint").text("Int: " + eint);
+    //ewis = ;
+    $("#ewis").text("wis: " + ewis);
+    //echa = ;
+    $("#echa").text("cha: " + echa);
 }
 
 fillStats();
 
 function compare() {
     console.log("hello");
+
 }
 
 fillStats();
